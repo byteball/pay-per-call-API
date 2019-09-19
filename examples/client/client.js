3d2e1e04767c9311a6fcf52aba8a9fb64bfa351a
papabyte <bcreze@protonmail.com>
2019-09-19 12:47:42 +0200

    examples

--- examples/client/client.js
+++ examples/client/client.js
@@ -11,6 +11,31 @@ payPerCallClient.startWhenReady().then(async function(){
 	} catch {
 		console.error("Coudln't create payment package, reason: " + error);
 	}
+	// objPaymentPackage can be encoded in JSON with JSON.stringify(objPaymentPackage) and transmitted to server alongside with your request
+
+
+	//let's say server sent back a refund in JSON format
+
+	// parse it
+
+	const objPaymentPackageFromServer = JSON.parse(`{
+		"version":"2.0t",
+		"signed_message":{
+			"payment_amount":5000,
+			"amount_spent":10000
+			,"period":1,
+			"aa_address":"PHA6RO4FVYKAY5QND77RF4V67ESHLZYJ"
+		},
+		"authors":[{"address":"CWNPFF6YAZWFEOZXKRB6527NZ47OFYRE",
+		"authentifiers":{"r":"DXRAAmoJMAZyV2S6djwfRWMiFOG2B3sxHiVtCIogjlQ6mg+rhp0fQWKAumBySsH8N3zJuCkNI7hM5zYz4UXCQA=="},
+		"definition":["sig",{"pubkey":"A6IMnIlrs+XI3TFjwIVdRnujE1692nLskboQugaKsMAQ"}]}]}`);
+
+	//it has to be treated with:
+	const objRefundFromServer = await payPerCallServer.verifyPaymentPackage(objPaymentPackageFromServer);
+	if (objRefundFromServer.error){
+		console.error(objRefundFromServer.error);
+	} else
+		console.error("Peer paid " + objRefundFromServer.amount + " in " + objRefundFromServer.asset + " using channel " + objRefundFromServer.aa_address);
 
 
 });
